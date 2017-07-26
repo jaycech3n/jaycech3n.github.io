a787420c828ed9c1cc43cad8c5717f41b5a94ea2
jaycech3n <jaycech3n@gmail.com>
2017-07-26 15:23:27 +0200

    Init

---
+++ make_index.py
@@ -0,0 +1,64 @@
+""" Build index from directory listing
+
+make_index.py [--dir </path/to/directory>]
+"""
+
+INDEX_TEMPLATE = r"""<!DOCTYPE html>
+<html>
+
+<head>
+    <title>${header}</title>
+</head>
+
+<body>
+
+<h2>${header}</h2>
+
+<ul>
+% for dname in dnames:
+    <li><a href="${dname}">${dname}/<a></li>
+% endfor
+% for fname in fnames:
+    <li><a href="${fname}">${fname}<a></li>
+% endfor
+</ul>
+
+</body>
+
+</html>
+"""
+
+EXCLUDED = ['.git', 'index.html', 'make_index.py']
+
+import os
+import argparse
+
+# May need to do "pip install mako"
+from mako.template import Template
+
+def process(directory):
+    print('Processing' + directory + '...')
+
+    header = os.path.basename(directory)
+    fnames = [name for name in sorted(os.listdir(directory))
+              if os.path.isfile(directory + '/' + name) and name not in EXCLUDED]
+    dnames = [name for name in sorted(os.listdir(directory))
+              if os.path.isdir(directory + '/' + name) and name not in EXCLUDED]
+
+    out = open(directory + '/index.html', 'w')
+    out.write(Template(INDEX_TEMPLATE).render(dnames=dnames, fnames=fnames, header=header))
+    out.close()
+
+    for dname in dnames:
+        process(directory + '/' + dname)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir")
+    args = parser.parse_args()
+    directory = args.dir if args.dir else os.getcwd()
+
+    process(directory)
+
+if __name__ == '__main__':
+    main()
