39a42f96be2a57e5c9317daf13dc758b1bcb84fc
jaycech3n <jaycech3n@gmail.com>
2017-07-27 00:15:24 +0200

    Style

--- make_index.py
+++ make_index.py
@@ -8,20 +8,23 @@ INDEX_TEMPLATE = r"""<!DOCTYPE html>
 
 <head>
     <title>${header}</title>
+    <link href="https://fonts.googleapis.com/css?family=Fira+Mono:400,500" rel="stylesheet">
     <style>
         body {
-          margin: 2em 8em 8em 4em;
-          background-color: #fcfcfc;
+          margin: 4em 4em 4em 4em;
+          background-color: #f6f6f6;
           color: #1f3a44;
           font-size: 16px;
+          font-family: 'Fira Mono', monospace;
         }
         footer {
           position: absolute;
           bottom: 1em;
-          border: 1px solid #1f3a44;
+          box-shadow: 0px 1px 3px 1px #a0a0a0;
+          background-color: #ffffff;
           padding: 1px;
           text-align: center;
-          font-size: 0.8em;
+          font-size: 0.7em;
         }
         footer a {
           color: #1f3a44;
@@ -36,17 +39,17 @@ INDEX_TEMPLATE = r"""<!DOCTYPE html>
 
 <ul>
 % for dname in dnames:
-    <li><a href="${dname}">${dname}/<a></li>
+    <li class="dirbullet"><a href="${dname}">${dname}/<a></li>
 % endfor
 % for fname in fnames:
-    <li><a href="${fname}">${fname}<a></li>
+    <li class="filebullet"><a href="${fname}">${fname}<a></li>
 % endfor
 </ul>
 
 <footer>
 <script src="https://use.fontawesome.com/fa49007a92.js"></script>
 Joshua Chen 2017
-<a href="about.html">
+<a href="${depth}about.html">
     <span class="fa fa-hand-spock-o" id="hi"
     onmouseover="javascript:document.getElementById('hi').setAttribute('class','fa fa-hand-lizard-o')"
     onmouseout="javascript:document.getElementById('hi').setAttribute('class','fa fa-hand-spock-o')"></span>
@@ -71,21 +74,21 @@ import argparse
 # May need to do "pip install mako"
 from mako.template import Template
 
-def process(directory, breadcrumb):
+def process(directory, header, breadcrumb, depth):
     print('Processing' + directory + '...')
 
-    header = os.path.basename(directory)
+    header = header if header else os.path.basename(directory)
     fnames = [name for name in sorted(os.listdir(directory))
               if os.path.isfile(directory + '/' + name) and name not in EXCLUDED]
     dnames = [name for name in sorted(os.listdir(directory))
               if os.path.isdir(directory + '/' + name) and name not in EXCLUDED]
 
     out = open(directory + '/index.html', 'w')
-    out.write(Template(INDEX_TEMPLATE).render(dnames=dnames, fnames=fnames, header=header, breadcrumb=breadcrumb))
+    out.write(Template(INDEX_TEMPLATE).render(dnames=dnames, fnames=fnames, header=header, breadcrumb=breadcrumb, depth=depth))
     out.close()
 
     for dname in dnames:
-        process(directory + '/' + dname, breadcrumb + header + ' &gt;')
+        process(directory + '/' + dname, '', breadcrumb + header + ' &gt;', depth + '../')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -93,7 +96,7 @@ def main():
     args = parser.parse_args()
     directory = args.dir if args.dir else os.getcwd()
 
-    process(directory, '')
+    process(directory, '', '', '')
 
 if __name__ == '__main__':
     main()
